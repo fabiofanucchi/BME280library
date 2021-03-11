0ec0909fa3cc721e1cc733c91e2ef6549757fbd8
Fabio Fanucchi <56686373+fabiofanucchi@users.noreply.github.com>
2021-03-11 11:56:59 +0100

    Add files via upload

--- BME280.cpp
+++ BME280.cpp
@@ -27,14 +27,15 @@
   @file "BME280/BME280.cpp"
 */
 
-#include"BME280.h"
+#include "BME280.h"
 
 /**@constructor BME280
    @brief object contructor
    @param none
    @return none
 */
-BME280::BME280() {
+BME280::BME280()
+{
 }
 
 /**@fn begin
@@ -42,19 +43,61 @@ BME280::BME280() {
    @param none
    @return true if sensor is correctly connected, false if not
 */
-bool BME280::begin() {
+bool BME280::begin()
+{
   // Wire begin
   Wire.begin();
   // check if sensor is connected, if not try the alternative address
-  if (!checkSensorID()) {
+  if (!checkSensorID())
+  {
     settings.I2CAddress = BME280_I2CADDRESS_ALTERNATE;
     // if also the alternative address is unavailable return false
-    if (!checkSensorID()) return false;
+    if (!checkSensorID())
+      return false;
   }
   // reset the sensor
   reset();
   // wait while sensor is updating
-  while (isUpdating()) delay(1);
+  while (isUpdating())
+    delay(1);
+  // read compensation coefficients
+  readCompensation();
+  // put in sleep mode(needed to write values)
+  writeMode(MODE_SLEEP);
+  // write settings
+  writetStandby(settings.tStandby);
+  writeFilter(settings.filter);
+  writePressureOS(settings.pressureOS);
+  writeTemperatureOS(settings.temperatureOS);
+  writeHumidityOS(settings.humidityOS);
+  // write user defined mode
+  writeMode(settings.mode);
+  // succes!
+  return true;
+}
+
+/**@fn begin
+   @brief begins Wire, checks sensor connection, resets sensor and writes settings
+   @param i2c sda pin, i2c scl pin
+   @return true if sensor is correctly connected, false if not
+*/
+bool BME280::begin(uint8_t sda, uint8_t scl)
+{
+  // Wire begin
+  Wire.begin(sda, scl);
+  // check if sensor is connected, if not try the alternative address
+  if (!checkSensorID())
+  {
+    settings.I2CAddress = BME280_I2CADDRESS_ALTERNATE;
+    // if also the alternative address is unavailable return false
+    if (!checkSensorID())
+      return false;
+  }
+  // reset the sensor
+  reset();
+  // wait while sensor is updating
+  while (isUpdating())
+    delay(1);
   // read compensation coefficients
   readCompensation();
   // put in sleep mode(needed to write values)
@@ -76,7 +119,8 @@ bool BME280::begin() {
    @param none
    @return none
 */
-void BME280::reset() {
+void BME280::reset()
+{
   // write the reset value in the reset register
   writeRegister(BME280_RST_REG, 0xB6);
 }
@@ -86,11 +130,13 @@ void BME280::reset() {
    @param none
    @return none
 */
-void BME280::forcedMeasurement() {
+void BME280::forcedMeasurement()
+{
   // set to forced mode (tell the sensor to take a measurement!)
   writeMode(MODE_FORCED);
   // wait while measuring and converting
-  while (isMeasuring()) delay(1);
+  while (isMeasuring())
+    delay(1);
 }
 
 /**@fn forcedMeasurement
@@ -99,7 +145,8 @@ void BME280::forcedMeasurement() {
    @param pointer to pressure, pointer to temperature, pointer to humidity
    @return none
 */
-void BME280::forcedMeasurement(float *pressure, float *temperature, float *humidity) {
+void BME280::forcedMeasurement(float *pressure, float *temperature, float *humidity)
+{
   // take a measurement
   forcedMeasurement();
   // read values
@@ -113,7 +160,8 @@ void BME280::forcedMeasurement(float *pressure, float *temperature, float *humid
    @param none
    @return pressure value
 */
-float BME280::readPressure() {
+float BME280::readPressure()
+{
   // read temperature first to get t_fine
   readTemperature();
   // read raw values from registers and compensate them (@see DS)
@@ -127,7 +175,8 @@ float BME280::readPressure() {
   var2 = var2 + (((int64_t)compensation.dig_P4) << 35);
   var1 = ((var1 * var1 * (int64_t)compensation.dig_P3) >> 8) + ((var1 * (int64_t)compensation.dig_P2) << 12);
   var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)compensation.dig_P1) >> 33;
-  if (var1 == 0) return 0; // avoid exception caused by division by 0
+  if (var1 == 0)
+    return 0; // avoid exception caused by division by 0
   p_acc = 1048576 - adc_P;
   p_acc = (((p_acc << 31) - var2) * 3125) / var1;
   var1 = (((int64_t)compensation.dig_P9) * (p_acc >> 13) * (p_acc >> 13)) >> 25;
@@ -141,15 +190,15 @@ float BME280::readPressure() {
    @param none
    @return temperature value
 */
-float BME280::readTemperature() {
+float BME280::readTemperature()
+{
   // read raw values from registers and compensate them (@see DS)
   uint8_t buffer[3];
   readRegisters(buffer, BME280_TEMPERATURE_MSB_REG, 3);
   int32_t adc_T = ((uint32_t)buffer[0] << 12) | ((uint32_t)buffer[1] << 4) | ((buffer[2] >> 4) & 0x0F);
   int64_t var1, var2;
   var1 = ((((adc_T >> 3) - ((int32_t)compensation.dig_T1 << 1))) * ((int32_t)compensation.dig_T2)) >> 11;
-  var2 = (((((adc_T >> 4) - ((int32_t)compensation.dig_T1)) * ((adc_T >> 4) - ((int32_t)compensation.dig_T1))) >> 12)
-          * ((int32_t)compensation.dig_T3)) >> 14;
+  var2 = (((((adc_T >> 4) - ((int32_t)compensation.dig_T1)) * ((adc_T >> 4) - ((int32_t)compensation.dig_T1))) >> 12) * ((int32_t)compensation.dig_T3)) >> 14;
   t_fine = var1 + var2;
   float ret = (t_fine * 5 + 128) >> 8;
   return ret / 100.0;
@@ -160,7 +209,8 @@ float BME280::readTemperature() {
    @param none
    @return humidity value
 */
-float BME280::readHumidity() {
+float BME280::readHumidity()
+{
   // read temperature first to get t_fine
   readTemperature();
   // read raw values from registers and compensate them (@see DS)
@@ -169,9 +219,7 @@ float BME280::readHumidity() {
   int32_t adc_H = ((uint32_t)buffer[0] << 8) | ((uint32_t)buffer[1]);
   int32_t var1;
   var1 = (t_fine - ((int32_t)76800));
-  var1 = (((((adc_H << 14) - (((int32_t)compensation.dig_H4) << 20) - (((int32_t)compensation.dig_H5) * var1))
-            + ((int32_t)16384)) >> 15) * (((((((var1 * ((int32_t)compensation.dig_H6)) >> 10) * (((var1 * ((int32_t)compensation.dig_H3)) >> 11)
-                                          + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) * ((int32_t)compensation.dig_H2) + 8192) >> 14));
+  var1 = (((((adc_H << 14) - (((int32_t)compensation.dig_H4) << 20) - (((int32_t)compensation.dig_H5) * var1)) + ((int32_t)16384)) >> 15) * (((((((var1 * ((int32_t)compensation.dig_H6)) >> 10) * (((var1 * ((int32_t)compensation.dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) * ((int32_t)compensation.dig_H2) + 8192) >> 14));
   var1 = (var1 - (((((var1 >> 15) * (var1 >> 15)) >> 7) * ((int32_t)compensation.dig_H1)) >> 4));
   var1 = (var1 < 0 ? 0 : var1);
   var1 = (var1 > 419430400 ? 419430400 : var1);
@@ -183,7 +231,8 @@ float BME280::readHumidity() {
    @param none
    @return none
 */
-void BME280::readCompensation() {
+void BME280::readCompensation()
+{
   compensation.dig_T1 = ((uint16_t)((readRegister(BME280_DIG_T1_MSB_REG) << 8) + readRegister(BME280_DIG_T1_LSB_REG)));
   compensation.dig_T2 = ((int16_t)((readRegister(BME280_DIG_T2_MSB_REG) << 8) + readRegister(BME280_DIG_T2_LSB_REG)));
   compensation.dig_T3 = ((int16_t)((readRegister(BME280_DIG_T3_MSB_REG) << 8) + readRegister(BME280_DIG_T3_LSB_REG)));
@@ -211,7 +260,8 @@ void BME280::readCompensation() {
    @param none
    @return true if CHIP_ID contains 0x60 or false
 */
-bool BME280::checkSensorID() {
+bool BME280::checkSensorID()
+{
   return (readRegister(BME280_CHIP_ID_REG) == 0x60);
 }
 
@@ -220,7 +270,8 @@ bool BME280::checkSensorID() {
    @param none
    @return true when sensor is copying, false when the copying is done
 */
-bool BME280::isUpdating() {
+bool BME280::isUpdating()
+{
   uint8_t stat = readRegister(BME280_STAT_REG);
   return (stat & 1 << 0);
 }
@@ -230,7 +281,8 @@ bool BME280::isUpdating() {
    @param none
    @return true when measuring, false when done
 */
-bool BME280::isMeasuring() {
+bool BME280::isMeasuring()
+{
   uint8_t stat = readRegister(BME280_STAT_REG);
   return (stat & (1 << 3));
 }
@@ -240,10 +292,11 @@ bool BME280::isMeasuring() {
    @param sensor mode
    @return none
 */
-void BME280::writeMode(sensorMode mode) {
+void BME280::writeMode(sensorMode mode)
+{
   uint8_t data = readRegister(BME280_CTRL_MEAS_REG);
-  data &= ~( (1 << 1) | (1 << 0) ); // clear bits 1, 0
-  data |= mode; // overwrite bits 1, 0
+  data &= ~((1 << 1) | (1 << 0)); // clear bits 1, 0
+  data |= mode;                   // overwrite bits 1, 0
   writeRegister(BME280_CTRL_MEAS_REG, data);
 }
 
@@ -252,10 +305,11 @@ void BME280::writeMode(sensorMode mode) {
    @param standby time
    @return none
 */
-void BME280::writetStandby(sensortStandby tStandby) {
+void BME280::writetStandby(sensortStandby tStandby)
+{
   uint8_t data = readRegister(BME280_CONFIG_REG);
-  data &= ~( (1 << 7) | (1 << 6) | (1 << 5) ); // clear bits 7, 6, 5
-  data |= tStandby << 5; // align with bits 7, 6, 5 and overwrite
+  data &= ~((1 << 7) | (1 << 6) | (1 << 5)); // clear bits 7, 6, 5
+  data |= tStandby << 5;                     // align with bits 7, 6, 5 and overwrite
   writeRegister(BME280_CONFIG_REG, data);
 }
 
@@ -264,10 +318,11 @@ void BME280::writetStandby(sensortStandby tStandby) {
    @param filter
    @return none
 */
-void BME280::writeFilter(sensorFilter filter) {
+void BME280::writeFilter(sensorFilter filter)
+{
   uint8_t data = readRegister(BME280_CONFIG_REG);
-  data &= ~( (1 << 4) | (1 << 3) | (1 << 2) ); // clear bits 4, 3, 2
-  data |= filter << 2; // align with bits 4, 3, 2 and overwrite
+  data &= ~((1 << 4) | (1 << 3) | (1 << 2)); // clear bits 4, 3, 2
+  data |= filter << 2;                       // align with bits 4, 3, 2 and overwrite
   writeRegister(BME280_CONFIG_REG, data);
 }
 
@@ -276,10 +331,11 @@ void BME280::writeFilter(sensorFilter filter) {
    @param pressure over sampling
    @return none
 */
-void BME280::writePressureOS(sensorSampling overSample) {
+void BME280::writePressureOS(sensorSampling overSample)
+{
   uint8_t data = readRegister(BME280_CTRL_MEAS_REG);
-  data &= ~( (1 << 4) | (1 << 3) | (1 << 2) ); // clear bits 4, 3, 2
-  data |= overSample << 2; // align with bits 4, 3, 2 and overwrite
+  data &= ~((1 << 4) | (1 << 3) | (1 << 2)); // clear bits 4, 3, 2
+  data |= overSample << 2;                   // align with bits 4, 3, 2 and overwrite
   writeRegister(BME280_CTRL_MEAS_REG, data);
 }
 
@@ -288,10 +344,11 @@ void BME280::writePressureOS(sensorSampling overSample) {
    @param temperature over sampling
    @return none
 */
-void BME280::writeTemperatureOS(sensorSampling overSample) {
+void BME280::writeTemperatureOS(sensorSampling overSample)
+{
   uint8_t data = readRegister(BME280_CTRL_MEAS_REG);
-  data &= ~( (1 << 7) | (1 << 6) | (1 << 5) ); // clear bits 7, 6, 5
-  data |= overSample << 5; // align with bits 7, 6, 5 and overwrite
+  data &= ~((1 << 7) | (1 << 6) | (1 << 5)); // clear bits 7, 6, 5
+  data |= overSample << 5;                   // align with bits 7, 6, 5 and overwrite
   writeRegister(BME280_CTRL_MEAS_REG, data);
 }
 
@@ -300,10 +357,11 @@ void BME280::writeTemperatureOS(sensorSampling overSample) {
    @param humidity over sampling
    @return none
 */
-void BME280::writeHumidityOS(sensorSampling overSample) {
+void BME280::writeHumidityOS(sensorSampling overSample)
+{
   uint8_t data = readRegister(BME280_CTRL_HUMIDITY_REG);
-  data &= ~( (1 << 2) | (1 << 1) | (1 << 0) ); // clear bits 2, 1, 0
-  data |= overSample; // align with bits 2, 1, 0 and overwrite
+  data &= ~((1 << 2) | (1 << 1) | (1 << 0)); // clear bits 2, 1, 0
+  data |= overSample;                        // align with bits 2, 1, 0 and overwrite
   writeRegister(BME280_CTRL_HUMIDITY_REG, data);
 }
 
@@ -312,7 +370,8 @@ void BME280::writeHumidityOS(sensorSampling overSample) {
    @param register's address, value to be written
    @return none
 */
-void BME280::writeRegister(uint8_t address, uint8_t value) {
+void BME280::writeRegister(uint8_t address, uint8_t value)
+{
   Wire.beginTransmission(settings.I2CAddress);
   Wire.write(address);
   Wire.write(value);
@@ -324,7 +383,8 @@ void BME280::writeRegister(uint8_t address, uint8_t value) {
    @param register's address
    @return register's value
 */
-uint8_t BME280::readRegister(uint8_t address) {
+uint8_t BME280::readRegister(uint8_t address)
+{
   Wire.beginTransmission(settings.I2CAddress);
   Wire.write(address);
   Wire.endTransmission();
@@ -338,13 +398,15 @@ uint8_t BME280::readRegister(uint8_t address) {
    @param output array, first address, number of registers
    @return none
 */
-void BME280::readRegisters(uint8_t* output, uint8_t address, uint8_t length) {
+void BME280::readRegisters(uint8_t *output, uint8_t address, uint8_t length)
+{
   Wire.beginTransmission(settings.I2CAddress);
   Wire.write(address);
   Wire.endTransmission();
   Wire.requestFrom(settings.I2CAddress, length);
   uint8_t i = 0;
-  while ((Wire.available()) && (i < length)) {
+  while ((Wire.available()) && (i < length))
+  {
     uint8_t value = Wire.read();
     *output = value;
     output++;
--- BME280.h
+++ BME280.h
@@ -30,313 +30,324 @@
 #ifndef BME280_h
 #define BME280_h
 
-#include<Arduino.h>
-#include<Wire.h>
+#include <Arduino.h>
+#include <Wire.h>
 
 #define BME280_I2CADDRESS 0x76
 #define BME280_I2CADDRESS_ALTERNATE 0x77
 
 /**REGISTERS
 */
-#define BME280_DIG_T1_LSB_REG        0x88 // ADC compensation
-#define BME280_DIG_T1_MSB_REG        0x89
-#define BME280_DIG_T2_LSB_REG        0x8A
-#define BME280_DIG_T2_MSB_REG        0x8B
-#define BME280_DIG_T3_LSB_REG        0x8C
-#define BME280_DIG_T3_MSB_REG        0x8D
-#define BME280_DIG_P1_LSB_REG        0x8E
-#define BME280_DIG_P1_MSB_REG        0x8F
-#define BME280_DIG_P2_LSB_REG        0x90
-#define BME280_DIG_P2_MSB_REG        0x91
-#define BME280_DIG_P3_LSB_REG        0x92
-#define BME280_DIG_P3_MSB_REG        0x93
-#define BME280_DIG_P4_LSB_REG        0x94
-#define BME280_DIG_P4_MSB_REG        0x95
-#define BME280_DIG_P5_LSB_REG        0x96
-#define BME280_DIG_P5_MSB_REG        0x97
-#define BME280_DIG_P6_LSB_REG        0x98
-#define BME280_DIG_P6_MSB_REG        0x99
-#define BME280_DIG_P7_LSB_REG        0x9A
-#define BME280_DIG_P7_MSB_REG        0x9B
-#define BME280_DIG_P8_LSB_REG        0x9C
-#define BME280_DIG_P8_MSB_REG        0x9D
-#define BME280_DIG_P9_LSB_REG        0x9E
-#define BME280_DIG_P9_MSB_REG        0x9F
-#define BME280_DIG_H1_REG            0xA1
-#define BME280_CHIP_ID_REG           0xD0 // chip ID == 0x60
-#define BME280_RST_REG               0xE0 // softreset: reset if == 0xB6
-#define BME280_DIG_H2_LSB_REG        0xE1
-#define BME280_DIG_H2_MSB_REG        0xE2
-#define BME280_DIG_H3_REG            0xE3
-#define BME280_DIG_H4_MSB_REG        0xE4
-#define BME280_DIG_H4_LSB_REG        0xE5
-#define BME280_DIG_H5_MSB_REG        0xE6
-#define BME280_DIG_H6_REG            0xE7
-#define BME280_CTRL_HUMIDITY_REG     0xF2 // ctrl_hum: [2:0] humidity OverSampling
-#define BME280_STAT_REG              0xF3 // status: [0] is 1 when sensor reads settings from EEPROM, [3] is 1 while measuring and converting
-#define BME280_CTRL_MEAS_REG         0xF4 // ctrl_meas: [1:0] mode, [4:2] pressure OverSampling, [7:5] temperature OverSampling
-#define BME280_CONFIG_REG            0xF5 // configuration: [0] SPI enable, [4:2] IIR filter, [7:5] tStandby (only in normal mode)
-#define BME280_PRESSURE_MSB_REG      0xF7 // pressure MSB
-#define BME280_PRESSURE_LSB_REG      0xF8 // pressure LSB
-#define BME280_PRESSURE_XLSB_REG     0xF9 // pressure XLSB
-#define BME280_TEMPERATURE_MSB_REG   0xFA // temperature MSB
-#define BME280_TEMPERATURE_LSB_REG   0xFB // temperature LSB
-#define BME280_TEMPERATURE_XLSB_REG  0xFC // temperature XLSB
-#define BME280_HUMIDITY_MSB_REG      0xFD // humidity MSB
-#define BME280_HUMIDITY_LSB_REG      0xFE // humidity LSB
-
+#define BME280_DIG_T1_LSB_REG 0x88 // ADC compensation
+#define BME280_DIG_T1_MSB_REG 0x89
+#define BME280_DIG_T2_LSB_REG 0x8A
+#define BME280_DIG_T2_MSB_REG 0x8B
+#define BME280_DIG_T3_LSB_REG 0x8C
+#define BME280_DIG_T3_MSB_REG 0x8D
+#define BME280_DIG_P1_LSB_REG 0x8E
+#define BME280_DIG_P1_MSB_REG 0x8F
+#define BME280_DIG_P2_LSB_REG 0x90
+#define BME280_DIG_P2_MSB_REG 0x91
+#define BME280_DIG_P3_LSB_REG 0x92
+#define BME280_DIG_P3_MSB_REG 0x93
+#define BME280_DIG_P4_LSB_REG 0x94
+#define BME280_DIG_P4_MSB_REG 0x95
+#define BME280_DIG_P5_LSB_REG 0x96
+#define BME280_DIG_P5_MSB_REG 0x97
+#define BME280_DIG_P6_LSB_REG 0x98
+#define BME280_DIG_P6_MSB_REG 0x99
+#define BME280_DIG_P7_LSB_REG 0x9A
+#define BME280_DIG_P7_MSB_REG 0x9B
+#define BME280_DIG_P8_LSB_REG 0x9C
+#define BME280_DIG_P8_MSB_REG 0x9D
+#define BME280_DIG_P9_LSB_REG 0x9E
+#define BME280_DIG_P9_MSB_REG 0x9F
+#define BME280_DIG_H1_REG 0xA1
+#define BME280_CHIP_ID_REG 0xD0 // chip ID == 0x60
+#define BME280_RST_REG 0xE0     // softreset: reset if == 0xB6
+#define BME280_DIG_H2_LSB_REG 0xE1
+#define BME280_DIG_H2_MSB_REG 0xE2
+#define BME280_DIG_H3_REG 0xE3
+#define BME280_DIG_H4_MSB_REG 0xE4
+#define BME280_DIG_H4_LSB_REG 0xE5
+#define BME280_DIG_H5_MSB_REG 0xE6
+#define BME280_DIG_H6_REG 0xE7
+#define BME280_CTRL_HUMIDITY_REG 0xF2    // ctrl_hum: [2:0] humidity OverSampling
+#define BME280_STAT_REG 0xF3             // status: [0] is 1 when sensor reads settings from EEPROM, [3] is 1 while measuring and converting
+#define BME280_CTRL_MEAS_REG 0xF4        // ctrl_meas: [1:0] mode, [4:2] pressure OverSampling, [7:5] temperature OverSampling
+#define BME280_CONFIG_REG 0xF5           // configuration: [0] SPI enable, [4:2] IIR filter, [7:5] tStandby (only in normal mode)
+#define BME280_PRESSURE_MSB_REG 0xF7     // pressure MSB
+#define BME280_PRESSURE_LSB_REG 0xF8     // pressure LSB
+#define BME280_PRESSURE_XLSB_REG 0xF9    // pressure XLSB
+#define BME280_TEMPERATURE_MSB_REG 0xFA  // temperature MSB
+#define BME280_TEMPERATURE_LSB_REG 0xFB  // temperature LSB
+#define BME280_TEMPERATURE_XLSB_REG 0xFC // temperature XLSB
+#define BME280_HUMIDITY_MSB_REG 0xFD     // humidity MSB
+#define BME280_HUMIDITY_LSB_REG 0xFE     // humidity LSB
 
 /**@enum sensorMode
    @brief contains modes values
 */
-enum sensorMode {
-  MODE_SLEEP = 0b00,
-  MODE_FORCED = 0b01, // also 0b10
-  MODE_NORMAL = 0b11
+enum sensorMode
+{
+   MODE_SLEEP = 0b00,
+   MODE_FORCED = 0b01, // also 0b10
+   MODE_NORMAL = 0b11
 };
 
 /**@enum sensortStandby
    @brief contains standby times values
 */
-enum sensortStandby {
-  STANDBY_MS_0_5 = 0b000,
-  STANDBY_MS_10 = 0b110,
-  STANDBY_MS_20 = 0b111,
-  STANDBY_MS_62_5 = 0b001,
-  STANDBY_MS_125 = 0b010,
-  STANDBY_MS_250 = 0b011,
-  STANDBY_MS_500 = 0b100,
-  STANDBY_MS_1000 = 0b101
+enum sensortStandby
+{
+   STANDBY_MS_0_5 = 0b000,
+   STANDBY_MS_10 = 0b110,
+   STANDBY_MS_20 = 0b111,
+   STANDBY_MS_62_5 = 0b001,
+   STANDBY_MS_125 = 0b010,
+   STANDBY_MS_250 = 0b011,
+   STANDBY_MS_500 = 0b100,
+   STANDBY_MS_1000 = 0b101
 };
 
 /**@enum sensorFilter
    @brief contains filters values
 */
-enum sensorFilter {
-  FILTER_X0 = 0b000,
-  FILTER_X2 = 0b001,
-  FILTER_X4 = 0b010,
-  FILTER_X8 = 0b011,
-  FILTER_X16 = 0b100
+enum sensorFilter
+{
+   FILTER_X0 = 0b000,
+   FILTER_X2 = 0b001,
+   FILTER_X4 = 0b010,
+   FILTER_X8 = 0b011,
+   FILTER_X16 = 0b100
 };
 
 /**@enum sensorSampling
    @brief contains samplings values
 */
-enum sensorSampling {
-  SAMPLING_X0 = 0b000,
-  SAMPLING_X1 = 0b001,
-  SAMPLING_X2 = 0b010,
-  SAMPLING_X4 = 0b011,
-  SAMPLING_X8 = 0b100,
-  SAMPLING_X16 = 0b101
+enum sensorSampling
+{
+   SAMPLING_X0 = 0b000,
+   SAMPLING_X1 = 0b001,
+   SAMPLING_X2 = 0b010,
+   SAMPLING_X4 = 0b011,
+   SAMPLING_X8 = 0b100,
+   SAMPLING_X16 = 0b101
 };
 
 /**@struct BME280Settings
    @brief contains settings of the sensor
    @note settings can be changed before @fn begin()
 */
-struct BME280Settings {
-  public:
-    uint8_t I2CAddress = BME280_I2CADDRESS;
+struct BME280Settings
+{
+public:
+   uint8_t I2CAddress = BME280_I2CADDRESS;
 
-    // settings
-    sensorMode mode = MODE_NORMAL;
-    sensortStandby tStandby = STANDBY_MS_0_5;
-    sensorFilter filter = FILTER_X0;
-    sensorSampling pressureOS = SAMPLING_X1;
-    sensorSampling temperatureOS = SAMPLING_X1;
-    sensorSampling humidityOS = SAMPLING_X1;
+   // settings
+   sensorMode mode = MODE_NORMAL;
+   sensortStandby tStandby = STANDBY_MS_0_5;
+   sensorFilter filter = FILTER_X0;
+   sensorSampling pressureOS = SAMPLING_X1;
+   sensorSampling temperatureOS = SAMPLING_X1;
+   sensorSampling humidityOS = SAMPLING_X1;
 };
 
 /**@struct BME280Compensation
    @brief contains ADC compensation values
 */
-struct BME280Compensation {
-  public:
-    uint16_t dig_T1;
-    int16_t dig_T2;
-    int16_t dig_T3;
+struct BME280Compensation
+{
+public:
+   uint16_t dig_T1;
+   int16_t dig_T2;
+   int16_t dig_T3;
 
-    uint16_t dig_P1;
-    int16_t dig_P2;
-    int16_t dig_P3;
-    int16_t dig_P4;
-    int16_t dig_P5;
-    int16_t dig_P6;
-    int16_t dig_P7;
-    int16_t dig_P8;
-    int16_t dig_P9;
+   uint16_t dig_P1;
+   int16_t dig_P2;
+   int16_t dig_P3;
+   int16_t dig_P4;
+   int16_t dig_P5;
+   int16_t dig_P6;
+   int16_t dig_P7;
+   int16_t dig_P8;
+   int16_t dig_P9;
 
-    uint8_t dig_H1;
-    int16_t dig_H2;
-    uint8_t dig_H3;
-    int16_t dig_H4;
-    int16_t dig_H5;
-    int8_t dig_H6;
+   uint8_t dig_H1;
+   int16_t dig_H2;
+   uint8_t dig_H3;
+   int16_t dig_H4;
+   int16_t dig_H5;
+   int8_t dig_H6;
 };
-
 
 /**@class BME280
    @brief main sensor class
 */
-class BME280 {
-  public:
-    BME280Settings settings;
-    BME280Compensation compensation;
+class BME280
+{
+public:
+   BME280Settings settings;
+   BME280Compensation compensation;
 
-    /**@constructor BME280
+   /**@constructor BME280
        @brief object contructor
        @param none
        @return none
     */
-    BME280();
+   BME280();
 
-    /**@fn begin
+   /**@fn begin
        @brief begins Wire, checks sensor connection, resets sensor and writes settings
        @param none
        @return true if sensor is correctly connected, false if not
     */
-    bool begin();
+   bool begin();
 
-    /**@fn reset
+   /**@fn begin
+       @brief begins Wire, checks sensor connection, resets sensor and writes settings
+       @param i2c sda pin, i2c scl pin
+       @return true if sensor is correctly connected, false if not
+    */
+   bool begin(uint8_t, uint8_t);
+
+   /**@fn reset
        @brief resets the sensor
        @param none
        @return none
     */
-    void reset();
+   void reset();
 
-    /**@fn forcedMeasurement
+   /**@fn forcedMeasurement
        @brief tells the sensor to take a measurement (needed only in MODE_FORCED)
        @param none
        @return none
     */
-    void forcedMeasurement();
+   void forcedMeasurement();
 
-    /**@fn forcedMeasurement
+   /**@fn forcedMeasurement
        @brief tells the sensor to take a measurement (needed only in MODE_FORCED)
     	         and writes values on 3 pointers given
        @param pointer to pressure, pointer to temperature, pointer to humidity
        @return none
     */
-    void forcedMeasurement(float*, float*, float*);
+   void forcedMeasurement(float *, float *, float *);
 
-    /**@fn readPressure
+   /**@fn readPressure
        @brief reads and calculate pressure value
        @param none
        @return pressure value
     */
-    float readPressure();
+   float readPressure();
 
-    /**@fn readTemperature
+   /**@fn readTemperature
        @brief reads and calculate temperature value
        @param none
        @return temperature value
     */
-    float readTemperature();
+   float readTemperature();
 
-    /**@fn readHumidity
+   /**@fn readHumidity
        @brief reads and calculate humidity value
        @param none
        @return humidity value
     */
-    float readHumidity();
+   float readHumidity();
 
-  private:
-    // global variable needed for temperature based pressure and humidity correction
-    int32_t t_fine;
+private:
+   // global variable needed for temperature based pressure and humidity correction
+   int32_t t_fine;
 
-    /**@fn readCompensation
+   /**@fn readCompensation
        @brief reads and stores in compensation trimming parameters
        @param none
        @return none
     */
-    void readCompensation();
+   void readCompensation();
 
-    /**@fn checkSensorID
+   /**@fn checkSensorID
        @brief checks if CHIP_ID register contains 0x60
        @param none
        @return true if CHIP_ID contains 0x60 or false
     */
-    bool checkSensorID();
+   bool checkSensorID();
 
-    /**@fn isUpdating
+   /**@fn isUpdating
        @brief checks if the sensor is copying NVM data to image registers
        @param none
        @return true when sensor is copying, false when done
     */
-    bool isUpdating();
+   bool isUpdating();
 
-    /**@fn isMeasuring
+   /**@fn isMeasuring
        @brief checks if the sensor is measuring and converting
        @param none
        @return true when measuring, false when done
     */
-    bool isMeasuring();
+   bool isMeasuring();
 
-    /**@fn writeMode
+   /**@fn writeMode
        @brief sets sensor mode (writing the corresponding value in the register)
        @param sensor mode
        @return none
     */
-    void writeMode(sensorMode);
+   void writeMode(sensorMode);
 
-    /**@fn writetStandby
+   /**@fn writetStandby
        @brief sets sensor standby time (writing the corresponding value in the register)
        @param standby time
        @return none
     */
-    void writetStandby(sensortStandby);
+   void writetStandby(sensortStandby);
 
-    /**@fn writeFilter
+   /**@fn writeFilter
        @brief sets sensor IIR filter (writing the corresponding value in the register)
        @param filter
        @return none
     */
-    void writeFilter(sensorFilter);
+   void writeFilter(sensorFilter);
 
-    /**@fn writePressureOS
+   /**@fn writePressureOS
        @brief sets pressure over sampling (writing the corresponding value in the register)
        @param pressure over sampling
        @return none
     */
-    void writePressureOS(sensorSampling);
+   void writePressureOS(sensorSampling);
 
-    /**@fn writeTemperatureOS
+   /**@fn writeTemperatureOS
        @brief sets temperature over sampling (writing the corresponding value in the register)
        @param temperature over sampling
        @return none
     */
-    void writeTemperatureOS(sensorSampling);
+   void writeTemperatureOS(sensorSampling);
 
-    /**@fn writeHumidityOS
+   /**@fn writeHumidityOS
        @brief sets humidity over sampling (writing the corresponding value in the register)
        @param humidity over sampling
        @return none
     */
-    void writeHumidityOS(sensorSampling);
+   void writeHumidityOS(sensorSampling);
 
-    /**@fn writeRegister
+   /**@fn writeRegister
        @brief writes in the register at the given address the given value
        @param register's address; value to be written
        @return none
     */
-    void writeRegister(uint8_t, uint8_t);
+   void writeRegister(uint8_t, uint8_t);
 
-    /**@fn readRegister
+   /**@fn readRegister
        @brief reads the value of the register at the given address
        @param register's address
        @return register's value
     */
-    uint8_t readRegister(uint8_t);
+   uint8_t readRegister(uint8_t);
 
-    /**@fn readRegisters
+   /**@fn readRegisters
        @brief reads the value of s given number of registers from the given address
        @param output array, first address, number of registers
        @return none
     */
-    void readRegisters(uint8_t*, uint8_t, uint8_t);
-
+   void readRegisters(uint8_t *, uint8_t, uint8_t);
 };
 
 #endif
